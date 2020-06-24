41d75512f8199b4114e2c91db19685f0670c3a96
Антон Глебездин <43610951+manulkote@users.noreply.github.com>
2020-06-24 21:06:36 +0300

    Lesson_2_4

---
+++ Lesson_2_4/test_client.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+
+class TestClient(TestCase):
+
+    def test_port(self):
+        self.assertEqual(type(port), type(0))
+
+    def test_port_value(self):
+        self.assertNotIn(port, range(1, 1024))
+
+    def test_responce(self):
+        self.assertNotEqual(obj, {})
